ca94b0b4b1e3122b2778a2200757864ba4295510
jakraphan <boszaanova@gmail.com>
2022-03-08 19:04:08 +0700

    2

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const port = 3000;
 const morgan = require('morgan');
 const debug = require('debug')('app');
 const path = require('path');
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
-
 app.use(express.static(path.join(__dirname,"/Public")));
+ 
+app.set("views","./src/views");
+app.set("view engine",  "ejs")
 
 app.get("/", (req,res) =>{
-    res.send("hello world");
+    res.render('index',{username: '8888'});
 
 })
 app.listen(port, () =>{
